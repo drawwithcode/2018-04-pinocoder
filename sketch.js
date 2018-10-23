d013df34981d6061cdc99a8a755b98d5ac49d8f6
pinocoder <41514345+pinocoder@users.noreply.github.com>
2018-10-24 00:17:49 +0200

    recupero consegna_4

--- sketch.js
+++ sketch.js
@@ -1,11 +1,43 @@
-function preload(){
-  // put preload code here
-}
+var x = 10;
+var y = 10;
+var xspeed = 10;
+var yspeed = 10;
 
 function setup() {
-  // put setup code here
+  //canvas big as the window
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+
 }
 
 function draw() {
-  // put drawing code here
+  push();
+  textFont('Arial');
+  textSize(24);
+  fill(0);
+  var myText = 'REDUCE THE SIZE OF THE WINDOW and move the mouse';
+  text(myText, 30, 100);
+  pop();
+
+
+  var d = dist(windowWidth / 4, windowHeight / 4, mouseX, mouseY);
+
+  //color and figure
+  fill(random(0, 255), random(0, 255), random(0, 255));
+  ellipse(x, y, d);
+
+  //bouncing horizontally
+  x = x + xspeed;
+
+  if (x > windowWidth || x < 0) {
+    xspeed = -xspeed;
+  }
+
+  //bouncing veritcally
+  y = y + yspeed;
+
+  if (y > windowHeight || y < 0) {
+    yspeed = -yspeed;
+  }
+
 }
